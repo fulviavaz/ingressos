9c884f6f53749323bbc0ced491fc8257ca7f8447
fulviavaz <fulviavaz@hotmail.com>
2024-01-18 17:14:52 -0300

    start the logic

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function comprar() {
+    let tipo = document.getElementById('tipo-ingresso')//guardar o valor do campo ingresso
+    let qtd = document.getElementById('qtd') //guardar o valor do campo quantidade
+
+    alert(tipo.value);
+    alert(qtd.value);
+}
