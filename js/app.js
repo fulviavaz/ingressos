053e44e9d1f28747e054e7ddbc47f02808303057
fulviavaz <fulviavaz@hotmail.com>
2024-01-23 13:35:20 -0300

    add logic to pista

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 function comprar() {
     let tipo = document.getElementById('tipo-ingresso')//guardar o valor do campo ingresso
-    let qtd = document.getElementById('qtd') //guardar o valor do campo quantidade
-
-    alert(tipo.value);
-    alert(qtd.value);
+    let qtd = parseInt(document.getElementById('qtd').value) //guardar o valor do campo quantidade
+    
+    if (tipo.value == 'pista') {
+        comprarPista(qtd);
+    }
 }
+function comprarPista(qtd) {
+    let qtdPista = parseInt( document.getElementById('qtd-pista').textContent);
+    if (qtd > qtdPista) {
+        alert('Quantidade indisponível para o tipo pista');
+    } else {
+        qtdPista = qtdPista = qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista
+        alert('Ingresso comprado com sucesso!');
+    }
+}
+
