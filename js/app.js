1534d5743aef844d539e6f99c0a2504f0293159f
fulviavaz <fulviavaz@hotmail.com>
2024-01-23 13:46:34 -0300

    fix logic

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ function comprar() {
     
     if (tipo.value == 'pista') {
         comprarPista(qtd);
+    } else if (tipo.value == 'superior') {
+        comprarSuperior(qtd);
+    } else (tipo.value == 'inferior'); {
+        comprarInferior(qtd);
     }
 }
 function comprarPista(qtd) {
@@ -16,4 +20,33 @@ function comprarPista(qtd) {
         alert('Ingresso comprado com sucesso!');
     }
 }
+
+function comprarSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (qtd > qtdSuperior) {
+        alert('Quantidade indisponível para o tipo superior');
+    } else {        
+        qtdSuperior = qtdSuperior - qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior
+        alert('Ingresso comprado com sucesso!');
+    }
+}
+    
+function comprarInferior(qtd) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if (qtd > qtdInferior) {
+        alert('Quantidade indisponível para o tipo inferior');
+    } else {
+        qtdInferior = qtdInferior - qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior
+        alert('Ingresso comprado com sucesso!');
+
+    }
+    }
+    
+
+
+
+
+
 
